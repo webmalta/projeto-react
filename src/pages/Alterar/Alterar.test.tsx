10ed113d9e84fdc32e88f1e426bb873d6e062534
marcosmalta <webmalta@gmail.com>
2025-02-02 10:43:08 -0300

    ajustando testes com histories

--- src/pages/Alterar/Alterar.test.tsx
+++ src/pages/Alterar/Alterar.test.tsx
@@ -1,18 +1,18 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import Alterar from '../Alterar';
+import Alterar from "../Alterar";
 import api from "services/api";
 import { BrowserRouter as Router } from "react-router-dom";
 
 jest.mock("services/api", () => ({
-    get: jest.fn(),
-    put: jest.fn()
-  }));
+  get: jest.fn(),
+  put: jest.fn(),
+}));
 
 const mockNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockNavigate,
-  useParams: () => ({ id: "1" })
+  useParams: () => ({ id: "1" }),
 }));
 
 describe("Componente Alterar", () => {
@@ -34,25 +34,32 @@ describe("Componente Alterar", () => {
     const mockDragon = {
       name: "Dragon 1",
       type: "Fire",
+      histories: ["History 1", "History 2"],
       createdAt: new Date().toISOString(),
     };
-
+  
     (api.get as jest.Mock).mockResolvedValueOnce({ data: mockDragon });
-
+  
     render(
       <Router>
         <Alterar />
       </Router>
     );
-
+  
     await waitFor(() => expect(screen.getByLabelText("Nome")).toHaveValue(mockDragon.name));
     expect(screen.getByLabelText("Tipo")).toHaveValue(mockDragon.type);
+  
+    const historiesTextArea = screen.getByLabelText("Descrição");
+    
+    const historiesString = mockDragon.histories.join("\n");
+    expect(historiesTextArea).toHaveValue(historiesString);
   });
 
   it("deve chamar handleChange quando os campos de entrada forem alterados", async () => {
     const mockDragon = {
       name: "Dragon 1",
       type: "Fire",
+      histories: ["History 1", "History 2"],
       createdAt: new Date().toISOString(),
     };
 
@@ -74,35 +81,43 @@ describe("Componente Alterar", () => {
 
   it("deve chamar salvarAlteracoes ao submeter o formulário", async () => {
     const mockDragon = {
-      name: "Dragon 1",
-      type: "Fire",
-      createdAt: new Date().toISOString(),
+        name: "Dragon 1",
+        type: "Fire",
+        createdAt: new Date().toISOString(),
+        histories: ["History 1", "History 2"],
     };
-  
+
     (api.get as jest.Mock).mockResolvedValueOnce({ data: mockDragon });
     (api.put as jest.Mock).mockResolvedValueOnce({});
-  
+
     render(
-      <Router>
-        <Alterar />
-      </Router>
+        <Router>
+            <Alterar />
+        </Router>
     );
-  
+
     await waitFor(() => expect(screen.getByLabelText("Nome")).toHaveValue(mockDragon.name));
-  
+
+    const nameInput = screen.getByLabelText("Nome");
+    fireEvent.change(nameInput, { target: { value: "Novo Dragão" } });
+
     const submitButton = screen.getByRole("button", { name: "Alterar" });
-  
     fireEvent.click(submitButton);
-  
-    await waitFor(() => expect(api.put).toHaveBeenCalledWith("/1", { name: "Dragon 1", type: "Fire" }));
-  
+
+    await waitFor(() => expect(api.put).toHaveBeenCalledWith("/1", {
+        ...mockDragon,
+        name: "Novo Dragão",
+        histories: ["History 1", "History 2"] 
+    }));
+
     await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/Home"));
-  });
+});
 
   it("deve navegar para Home ao clicar no botão cancelar", async () => {
     const mockDragon = {
       name: "Dragon 1",
       type: "Fire",
+      histories: ["History 1", "History 2"],
       createdAt: new Date().toISOString(),
     };
 
